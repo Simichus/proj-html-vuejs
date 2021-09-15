e4729ba16695baef9b27f8e54779ab86b79ee54f
Simichus <simonefilippobilardo@gmail.com>
2021-09-15 12:41:26 +0200

    Completato header

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <a href="#" class="fs-5">{{text}}</a>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:['text']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/_vars.scss';
+
+a{
+    color: $white;
+    padding: 7px 15px;
+    background-color: $secondary-color;
+    border-radius: 40px;
+    transition: background-color .25s;
+
+    &:hover{
+        background-color: $secondary-color-hover;
+    }
+}
+</style>
--- src/components/HeaderJumbo.vue
+++ src/components/HeaderJumbo.vue
@@ -2,9 +2,22 @@
   <div id="header-jumbo">
       <div class="jumbo-nav py-3">
         <div class="container d-flex justify-content-between align-items-center">
-            <img src="@/assets/img/avada-drivers-logo-2x-300x58.png" alt="">  
+            <img src="@/assets/img/avada-drivers-logo-2x-300x58.png" alt=""> 
+            <div class="d-flex justify-content-between align-items-center">
             <Navbar :navbar-voices="navbarVoices"/>
+            <Button text="Book Now"/>
+            </div> 
         </div>
+            <div id="info-boxes">
+                <div class="info-box my-2 d-flex flex-column justify-content-center align-items-center">
+                    <i class="fab fa-atlassian"></i>
+                    <div>Demos</div>
+                </div>
+                <div class="info-box my-2 d-flex flex-column justify-content-center align-items-center">
+                    <div class="sale fs-4"><span>$</span>39</div>
+                    <div>On Sale</div>
+                </div>
+            </div>    
       </div>
       <div class="container jumbo-text d-flex flex-column justify-content-center">
           <h1>Drive with Avada</h1>
@@ -15,11 +28,13 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Button from '@/components/Button.vue'
 
 export default {
 name:'HeaderJumbo',
 components:{
-    Navbar
+    Navbar,
+    Button,
 },
 data(){
     return{
@@ -64,6 +79,41 @@ data(){
 
     .jumbo-nav{
         background-color: rgba($color: $main-color, $alpha: 0.6);
+        position: relative;
+
+
+        #info-boxes{
+            position: absolute;
+            right: 1%;
+            bottom: -150px;
+
+            .info-box{
+                color: $black;
+                height: 75px;
+                width: 75px;
+                background-color: $white;
+                border-radius: 10px;
+                box-shadow: -5px 5px 10px rgba($color: #000000, $alpha: 0.2);
+
+                i{
+                    color: $white;
+                    background-color: $black;
+                    padding: 4px;
+                    border-radius: 6px;
+                }
+
+                .sale{
+                    color: $secondary-color;
+                    font-weight: bold;
+
+                    span{
+                        font-size: 15px;
+                        vertical-align: text-top;
+                    }
+                }
+            }
+        }
+        
     }
 
     .jumbo-text{
