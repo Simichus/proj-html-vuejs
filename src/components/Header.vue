33e0e3b9012a4b48640787845ba06bc1f2901575
Simichus <simonefilippobilardo@gmail.com>
2021-09-15 11:20:27 +0200

    Aggiunto Header + Header top

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+      <HeaderTop/>
+  </header>
+</template>
+
+<script>
+import HeaderTop from "@/components/HeaderTop.vue";
+export default {
+name: 'Header',
+components:{
+    HeaderTop
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header-top">
+      <div class="container d-flex justify-content-between align-items-center py-3">
+          <h3>We have a 95% Succesful Pass rate!</h3>
+          <h3>
+              <i class="fas fa-phone me-2"></i>
+              <span class="me-2">Give us a call to book your tuition!</span>
+              <span class="phone-number">1-800-555-555</span>
+          </h3>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderTop',
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/_vars.scss';
+
+.header-top{
+    background-color: $bg-gray;
+    color: $light-gray;
+
+    h3{
+        font-size: 10px;
+        margin: 0;
+    }
+}
+</style>
